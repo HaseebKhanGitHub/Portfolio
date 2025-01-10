6162d12d46f09fb5a2cb7939a2bc3be5bddf1b44
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2025-01-10 23:15:52 +0500

    commit

---
+++ lib/Views/homepage.dart
@@ -0,0 +1,30 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 250,vertical: 30),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 80,
+                width: 80,
+              )
+
+          ],)
+        ],),
+
+      ),
+    );
+  }
+}
